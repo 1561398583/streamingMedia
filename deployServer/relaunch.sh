f660afaab4dda727fc373dfdc514426798de99ef
YX <yxofyx@163.com>
2020-10-19 21:30:12 +0800

    shell

--- deployServer/relaunch.sh
+++ deployServer/relaunch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo su root
-cd ~/streamingMedia/
+cd /home/admin/streamingMedia/
 git pull https://github.com/1561398583/streamingMedia.git
-chmod 777 mediaServer/mediaServer
+chmod 777 /home/admin/streamingMedia/mediaServer/mediaServer
 
